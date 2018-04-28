86dcd7b1daa5594536fc250e15b0ecd9c9a830a8
zengzuozuo <zengzuozuo@163.com>
2018-04-28 16:33:00 +0800

    提交dist

--- dist/static/lang/en.js
+++ dist/static/lang/en.js
@@ -4,25 +4,70 @@ module.exports={
   	English: 'English',
   	
     login: 'CHAT ON',
-    logout: 'Logging Out',
+    logout: 'Logout',
     miliao: 'Secret Chat',
-    miliaoPhrase: 'A top-level security anonymous chat software based on block chain',
+    miliaoPhrase: 'Top security anonymous chat software based on block chain',
     loginInputTip: 'Please enter your SMS password statement for login',
     registerText: 'No account?Create a new anonymous account',
     safety: 'Safety',
     copy: 'copy',
-    copyaddress: 'copy Address',
+    copyaddress: 'copy',
     copySuccess: 'Copy success',
     Connectionclosed: 'Connection closed',
+    login1: 'enter your key',
+    login2: 'Please enter',
+    login3: 'Forget your key',
+    login4: 'Tip',
+    login5: 'Will it not be able to decrypt the previous information before it is regenerated?',
+    login6: 'Login environment exception',
+    login7: 'The secret key you entered cannot be empty',
 
     //index
     my: 'My',
     chat: 'Chats',
     set: 'Settings',
 
+    //wallet
+    wdye: 'Balance',
+    wallet1: 'Send the address to someone else and someone can send the appropriate information to your address via a private chat',
+    wallet2: 'pieces',
+    wallet3: 'Details of the membership fee:Notes: The sac you top up is charged by two sections -- the public network of the Ethereum on the one hand, and our platform on the other to maintain the operation.',
+    wallet4: 'Spike',
+    wallet5: 'Purchase Succeeds',
+
+    //register
+    register1: 'Your secret key has been generated',
+    register2: 'important note',
+    register3: 'This key will be used to encrypt and decrypt your chat information. Be sure to save your secret key.If lost, no way to recover it.Please do not disclose to third parties, otherwise your information may be leaked, we are not responsible!',
+    register6: "I've saved it. Go to chat",
+
+    //chatlist
+    chatlist1: 'Start New Chat',
+    chatlist2: 'Recipient address',
+    chatlist3: 'Enter',
+    chatlist4: 'cancel',
+    chatlist5: 'Please enter',
+    chatlist6: 'The address cannot be empty',
+    chatlist7: "You can't send it to yourself",
+    chatlist8: 'nothing',
+
+    //chatin
+    chatin1: 'Anonymous User',
+    chatin2: 'Type your message',
+    chatin3: 'The send content cannot be empty',
+    chatin4: 'The character length exceeds the limit',
+
     //tip
     dlzhbnwk: 'The login account cannot be empty',
     tip01: 'The browser does not support automatic replication',
+
+    //set
+    set1: 'After 5 minutes, the application will exits automatically, and the next login must enter the key.',
+    set2: 'Language',
+
+    specification1: 'ALL content you sent are encrypted using TOP Encryption algorithm and saved on Blockchain, which shall NEVER store on any other platform.',
+    specification2: 'You privacy message will be kept so far, by keeping you private key secrecy. Even though one use the newest supercomputer, he cannot decipher your message.',
+    specification3: 'You can inquire the calling record of your chat in Blockchain Browser. ',
     
     welcome: "Welcome"
   }
--- dist/static/lang/zh.js
+++ dist/static/lang/zh.js
@@ -5,7 +5,7 @@ module.exports={
   	//login
     login: '开启聊天',
     logout: '安全退出',
-    miliao: '密聊',
+    miliao: '密信',
     miliaoPhrase: '一款基于区块链的顶级安全匿名聊天软件',
     loginInputTip: '请输入您的短信密码语句进行登录',
     registerText: '没有帐号？创建一个新的匿名帐号',
@@ -14,6 +14,13 @@ module.exports={
     copyaddress: '复制地址',
     copySuccess: '复制成功',
     Connectionclosed: '连接已关闭',
+    login1: '请输入您的秘钥',
+    login2: '请输入',
+    login3: '忘记秘钥',
+    login4: '提示',
+    login5: '重新生成后将无法解密在此之前的信息，确定继续？',
+    login6: '登录环境异常',
+    login7: '您输入的秘钥不能为空',
 
     
     //index
@@ -21,9 +28,47 @@ module.exports={
     chat: '聊天',
     set: '设置',
 
+    //wallet
+    wdye: '您的余额',
+    wallet1: '把地址发给他人，他人即可通过密聊发送相应的信息到您的地址',
+    wallet2: '条',
+    wallet3: '备注：您充值的sac，一部分是以太坊公网收取的，一部分是我们的运营费用。',
+    wallet4: '限时免费',
+    wallet5: '购买成功',
+
+    //register
+    register1: '您的秘钥已经生成',
+    register2: '重要提示',
+    register3: '此密钥将用户加解密您的聊天信息。请务必保存您的秘钥.如果丢失, 将无法找回 ！请不要泄露给第三方，否则您的信息可能被泄露,我们概不负责！',
+    register6: '我已保存，去聊天!',
+
+    //chatlist
+    chatlist1: '开启新的聊天',
+    chatlist2: '请输入对方地址',
+    chatlist3: '确定',
+    chatlist4: '取消',
+    chatlist5: '请输入',
+    chatlist6: '对方地址不能为空',
+    chatlist7: '无法向自己发送',
+    chatlist8: '无',
+
+    //chatin
+    chatin1: '匿名用户',
+    chatin2: '请输入您的消息',
+    chatin3: '发送内容不能为空',
+    chatin4: '字符长度超出限制',
+
     //tip
     dlzhbnwk: '登录账号不能为空',
     tip01: '该浏览器不支持自动复制',
+
+    specification1: '您所有聊天内容，都是使用高密级算法加密并保存到区块链，不会存放在任何其他平台。',
+    specification2: '保护好您的私人密钥，隐私就得到最强的保护。当代超级计算机也无法破解您的聊天内容。',
+    specification3: '您可以使用区块链浏览器查询聊天的呼叫信息。',
+
+    //set
+    set1: '5分钟后，自动安全退出应用，下一次登录需要重新输入密钥。',
+    set2: '语言',
     
     welcome: "Welcome"
   }
